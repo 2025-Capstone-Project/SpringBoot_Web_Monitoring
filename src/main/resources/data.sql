6ba1364d96a1a0c921e19f0621251c0d1bcc2460
mintball9543 <minseokcomputer@gmail.com>
2025-09-03 20:14:29 +0900

    Revert::Bug fixed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (name, email) VALUES ('홍길동','hong@gmail.com');
-INSERT INTO users (name, email) VALUES ('김철수','kim@gmail.com');
-INSERT INTO users (name, email) VALUES ('이영희','lee@gmail.com');
+INSERT INTO users (username, password, name, email) VALUES ('hong', 'pass123', '홍길동', 'hong@gmail.com');
+INSERT INTO users (username, password, name, email) VALUES ('kim', 'pass123', '김철수', 'kim@gmail.com');
+INSERT INTO users (username, password, name, email) VALUES ('lee', 'pass123', '이영희', 'lee@gmail.com');
