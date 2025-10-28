0d663fe4ac816ef534d492e100ba3c949bb6d29a
mintball9543 <minseokcomputer@gmail.com>
2025-10-28 17:06:15 +0900

    Refresh .gitignore

--- build/resources/main/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (username, password, name, email, role) VALUES ('hong', '$2a$10$pPGaxmUb6cMC6rltCdrC6.zZHHN0z7e2ETP6nWhi2ahVUMfl4Kfym', '홍길동', 'hong@gmail.com', 'ADMIN');
-INSERT INTO users (username, password, name, email, role) VALUES ('kim', '$2a$10$2jV6lOnqlom1PLakALU7ZuKBWWKtoj.vecEPMrryUEFrf4auBxR1e', '김철수', 'kim@gmail.com', 'USER');
-INSERT INTO users (username, password, name, email, role) VALUES ('lee', '$2a$10$Rrhsf0M.Af14z27F7qNVpuskzw81X1leehkhrvnPhLsTabs48A9cG', '이영희', 'lee@gmail.com', 'USER');
---password: pass123
